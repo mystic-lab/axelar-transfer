96877c1bf4dba7e196ce76406ee0fded35f15040
Joe S <schnetzlerjoe@gmail.com>
2022-12-22 13:19:28 -0500

    Bug init + install fixes

--- network/agoric/agoric.sh
+++ network/agoric/agoric.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 # Install the specialized, agoric sdk for ICA
 cd $HOME
 yes | rm -r agoric-sdk
 git clone https://github.com/schnetzlerjoe/agoric-sdk
-cd agoric-sdk
-yarn install
-yarn build
+cd agoric-sdk && yarn install && yarn build
 yarn link-cli ~/bin/agoric
 cd ./packages/cosmic-swingset && make
--- network/axelar/axelar.sh
+++ network/axelar/axelar.sh
@@ -6,12 +6,12 @@ cd $HOME
 mkdir binaries && cd binaries
 
 cd $HOME
-yes | rm -r osmosis
+yes | rm -r axelar-core
 git clone https://github.com/pitalco/axelar-core
 cd axelar-core
 git checkout upgrade-ibc3
-make install
-sudo mv $GOPATH/bin/axelard /usr/local/bin
+make build
+sudo mv ./bin/axelard /usr/local/bin
 
 # install tofnd
 wget https://github.com/axelarnetwork/tofnd/releases/download/$TOFND_RELEASE/tofnd-linux-amd64-$TOFND_RELEASE
